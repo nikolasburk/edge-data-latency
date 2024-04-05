7bbf1f0a83824b55afc040dc775518be122a8d4e
Nikolas Burk <nikolas.burk@gmail.com>
2024-04-05 09:38:07 +0200

    adjust function params for neon

--- pages/api/node/neon-global.ts
+++ pages/api/node/neon-global.ts
@@ -4,11 +4,13 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const start = Date.now();
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const time = Date.now();
-
-  const sql = neon(process.env.NEON_DATABASE_URL);
+  
+  
   const { count } = req.query
+  
+  const sql = neon(process.env.NEON_DATABASE_URL);
 
+  const time = Date.now();
   let data = null;
   for (let i = 0; i < toNumber(count); i++) {
     data = await sql`
@@ -17,14 +19,17 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       LIMIT 10`;
   }
 
-  return Response.json({
-    data,
-    queryDuration: Date.now() - time,
-    invocationIsCold: start === time,
-    invocationRegion: (req.headers.get("x-vercel-id") ?? "").split(":")[1] || null,
-  }, {
-    headers: { "x-edge-is-cold": start === time ? "1" : "0" },
-  });
+  return res.status(200).json(
+    {
+      data,
+      queryDuration: Date.now() - time,
+      invocationIsCold: start === time,
+      // invocationRegion: (req.headers.get("x-vercel-id") ?? "").split(":")[1] || null,
+    }
+    // {
+    //   headers: { "x-edge-is-cold": start === time ? "1" : "0" },
+    // }
+  );
 }
 
 // convert a query parameter to a number, applying a min and max, defaulting to 1
