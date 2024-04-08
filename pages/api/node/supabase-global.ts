5e7313834421dfcac9a0e3e7eebdf02a051d3e12
Nikolas Burk <nikolas.burk@gmail.com>
2024-04-08 17:17:04 +0200

    fix supabase issue

--- pages/api/node/supabase-global.ts
+++ pages/api/node/supabase-global.ts
@@ -5,12 +5,9 @@ import type { NextApiRequest, NextApiResponse } from "next";
 const start = Date.now();
 
 // 2. initialize DB client
-console.log(`process.env.SUPABASE_DATABASE_URL: `, process.env.SUPABASE_DATABASE_URL);
+console.log(`process.env.SUPABASE_URL: `, process.env.SUPABASE_URL);
 console.log(`init supabase js client`);
-const supabase = createClient<Database>(
-  process.env.SUPABASE_DATABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
-);
+const supabase = createClient<Database>(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(`url: `, req.url);
@@ -25,21 +22,16 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   // 5. run queries `count` times
   let data = null;
   for (let i = 0; i < toNumber(count); i++) {
-    const response = await supabase
-      .from("employees")
-      .select("emp_no,first_name,last_name")
-      .limit(10);
+    const response = await supabase.from("employees").select("emp_no,first_name,last_name").limit(10);
     data = response.data;
   }
 
   // 6. return response
-  return res.status(200).json(
-    {
-      data,
-      queryDuration: Date.now() - time,
-      invocationIsCold: start === time,
-    }
-  );
+  return res.status(200).json({
+    data,
+    queryDuration: Date.now() - time,
+    invocationIsCold: start === time,
+  });
 }
 
 // convert a query parameter to a number, applying a min and max, defaulting to 1
@@ -47,7 +39,6 @@ function toNumber(queryParam: string | string[] | null, min = 1, max = 5) {
   const num = Number(queryParam);
   return Number.isNaN(num) ? 1 : Math.min(Math.max(num, min), max);
 }
-
 
 // Auto generated types: https://supabase.com/docs/guides/api/generating-types
 interface Database {
